b3348de7b1c2c43dba73e394b27ec6ec44ec70dc
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2022-07-01 23:13:57 +0300

    Simplify command line options

--- solex_read.py
+++ solex_read.py
@@ -34,10 +34,10 @@ def process_video(filename: str) -> np.ndarray:
 
 @click.command()
 @click.option('--save', is_flag=True, default=False)
-@click.option('--show/--no-show', default=True)
-@click.option('--crop/--no-crop', default=True)
+@click.option('--no-show', is_flag=True, default=False)
+@click.option('--no-crop', is_flag=True, default=False)
 @click.argument('files', nargs=-1)
-def solex_read(files, save, show, crop):
+def solex_read(files, save, no_show, no_crop):
     if os.name == 'nt':
         # Windows does not automatically expand wildcards, so that has to be done with the glob module.
         # Note that technically Windows allows square brackets in filenames, so this solution
@@ -46,15 +46,15 @@ def solex_read(files, save, show, crop):
     for f in files:
         click.echo(f'Processing: {f}')
         result = process_video(f)
-        if crop:
+        if not no_crop:
             result = crop_image(result)
         click.echo(f'Result image size: {result.shape[0]}x{result.shape[1]} pixels')
-        if show:
-            show_image(result)
         if save:
             new_file_path = pathlib.Path(f).with_suffix('.png')
             click.echo(f'Saving result to: {new_file_path}')
             imageio.imwrite(new_file_path, result.astype(np.uint16))
+        if not no_show:
+            show_image(result)
 
 
 if __name__ == '__main__':
