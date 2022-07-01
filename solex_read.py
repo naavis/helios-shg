1caaef780700abb34bf434911cedffa8e60b1552
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2022-07-02 00:06:52 +0300

    Add parameters for flipping image horizontally and/or vertically

--- solex_read.py
+++ solex_read.py
@@ -17,8 +17,10 @@ from solex.processor import process_video
 @click.option('--output-size', type=int, help='Desired image side length in pixels. Handy for matching the size of '
                                               'several images!')
 @click.option('--ref-frame', type=int, help='Reference frame for finding absorption line, defaults to middle frame')
+@click.option('--flipv', is_flag=True, help='Flip result vertically.')
+@click.option('--fliph', is_flag=True, help='Flip result horizontally.')
 @click.argument('files', nargs=-1)
-def solex_read(files, save, no_show, no_crop, ref_frame, output_size):
+def solex_read(files, save, no_show, no_crop, ref_frame, output_size, flipv, fliph):
     """Processes Sol'Ex spectroheliograph videos into narrowband still images."""
     if os.name == 'nt':
         # Windows does not automatically expand wildcards, so that has to be done with the glob module.
@@ -35,6 +37,10 @@ def solex_read(files, save, no_show, no_crop, ref_frame, output_size):
             new_file_path = pathlib.Path(f).with_suffix('.png')
             click.echo(f'Saving result to: {new_file_path}')
             imageio.imwrite(new_file_path, result.astype(np.uint16))
+        if flipv:
+            result = np.flipud(result)
+        if fliph:
+            result = np.fliplr(result)
         if not no_show:
             show_image(result)
         click.echo('')
